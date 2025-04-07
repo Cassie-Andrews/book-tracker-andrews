6a39444a87419122c298921f76807ae3e63c9f26
Cassie Andrews <candrews1@ufl.edu>
2025-04-07 12:44:10 -0400

    update schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -5,3 +5,21 @@ CREATE TABLE users (
   username VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(200) NOT NULL
 );
+
+CREATE TABLE books (
+  id INT NOT NULL VARCHAR(255) PRIMARY KEY UNIQUE, 
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  genre VARCHAR(100),
+  cover VARCHAR(255)
+);
+
+CREATE TABLE user_books (
+  id INT NOT NULL VARCHAR(255) PRIMARY KEY UNIQUE,
+  user_id INT,
+  book_id INT,
+  bookshelf ENUM('want_to_read', 'reading', 'read') NOT NULL,
+  rating INT,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE,
+);
