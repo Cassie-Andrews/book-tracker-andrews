7f047ba85f5a95c564dd30d59b29708463d60771
Cassie Andrews <candrews1@ufl.edu>
2025-04-07 12:58:08 -0400

    update schema 4

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,11 +3,11 @@ USE mysql_project_db;
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(200) NOT NULL
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE books (
-  id INT NOT NULL VARCHAR(255) PRIMARY KEY UNIQUE, 
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
   title VARCHAR(255) NOT NULL,
   author VARCHAR(255) NOT NULL,
   genre VARCHAR(100),
@@ -15,7 +15,7 @@ CREATE TABLE books (
 );
 
 CREATE TABLE user_books (
-  id INT NOT NULL VARCHAR(255) PRIMARY KEY UNIQUE,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   user_id INT,
   book_id INT,
   bookshelf ENUM('want_to_read', 'reading', 'read') NOT NULL,
