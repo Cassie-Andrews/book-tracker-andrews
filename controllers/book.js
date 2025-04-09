a2539fa391d0e2af40e3ae9fc992aba20b2d84d4
Cassie Andrews <candrews1@ufl.edu>
2025-04-09 17:57:15 -0400

    update book controller

--- controllers/book.js
+++ controllers/book.js
@@ -3,19 +3,17 @@ const fetch = require('node-fetch')
 const {getOpenLibraryData} = require('../api/bookApi')
 
 async function searchBooks(req, res) {
-    try {
-        const {query} = req.query
-        
-        if (!query) {
-            return res.redirect('/')
-        }
-        
-        const books = await book.searchBooks(query)
-        
-        res.render("index", {books})
-        
+    const {query} = req.query
+    
+    if (!query) {
+        return res.redirect('/')
+    }
+
+    try {    
+        const books = await getOpenLibraryData(query)
+        res.render("index", {books, isLoggedIn: req.session.isLoggedIn})
     } catch (err) {
-        res.status(500).send(err.message)
+        res.status(500).send("Error fetching books: " + err.message)
     }
 }
 
