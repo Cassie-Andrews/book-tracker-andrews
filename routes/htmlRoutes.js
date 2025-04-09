aac83f8e4b383cf301513022e51fb28cb0c7ce7c
Cassie Andrews <candrews1@ufl.edu>
2025-04-08 21:24:08 -0400

    update htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,36 @@
 const router = require("express").Router();
-const controllers = require("../controllers");
+const {authController, bookController} = require("../controllers");
 const checkAuth = require("../middleware/auth");
+
+// book list
+router.get("/", checkAuth, async (req, res) => {
+  try {
+    const books = await bookController.getBooks()
+    res.render("index", { books, isLoggedIn: req.session.isLoggedIn });
+  } catch (err) {
+    res.status(500).send(err.message)
+  } 
+});
+
 
 router.get("/", ({ session: { isLoggedIn } }, res) => {
   res.render("index", { isLoggedIn });
 });
 
+// login
 router.get("/login", async (req, res) => {
   if (req.session.isLoggedIn) return res.redirect("/");
   res.render("login", { error: req.query.error });
 });
 
+// signup
 router.get("/signup", async (req, res) => {
   if (req.session.isLoggedIn) return res.redirect("/");
   res.render("signup", { error: req.query.error });
 });
 
+
+// private route
 router.get("/private", checkAuth, ({ session: { isLoggedIn } }, res) => {
   res.render("protected", { isLoggedIn });
 });
