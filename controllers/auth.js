5e47b20264e8e1b1d9c0d5d757d94c8b4c744c1c
Cassie Andrews <candrews1@ufl.edu>
2025-04-14 17:20:56 -0400

    stopping point

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const indexModel = require("../models/indexModel");
 const userModel = require("../models/userModel");
 
 
+
+
 async function login(req, res) {
   try {
     const { username, password } = req.body;
@@ -29,8 +31,41 @@ async function login(req, res) {
   }
 }
 
+
+
+
+async function signup(req, res) {
+  try {
+    const { username, password } = req.body;
+
+    // must include a username AND password, return error if not
+    if (!username || !password)
+      return res.redirect("/signup?error=must include username and password")
+
+    // check if user already exists - if yes, return error, redirect to login
+    const existingUser = await userModel.findByUsername(username);
+    if (existingUser)
+        return res.redirect("/signup?error=username already exists")
+
+    // if username and password are valid AND new - create a new user
+    const newUser = await userModel.create(username, password)
+
+    req.session.isLoggedIn = true;
+    req.session.userId = newUser.id;
+    req.session.save(() => res.redirect("/private"));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+
+
+
 async function logout(req, res) {
   req.session.destroy(() => res.redirect("/"));
 }
 
-module.exports = { login, logout };
+
+
+
+module.exports = { login, logout, signup };
